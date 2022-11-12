2ce276d7c292eeb4f40ddbcea372cb977679e243
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-12 15:51:38 +0800

    added youtube

---
+++ bot/youtube.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+let comments = [];
+let i = 0;
+
+get("");
+
+function get(continuation) {
+  fetch(
+    `https://vid.puffyan.us/api/v1/comments/5q0gJs2eYB4?continuation=${continuation}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      i++;
+      comments = comments.concat(data.comments);
+      get(data.continuation);
+      if (i === 25) {
+        console.log(`Downloaded ${comments.length} comments.`);
+        fs.writeFileSync("comments.json", JSON.stringify(comments, null, 2));
+        process.exit();
+      }
+    });
+}
