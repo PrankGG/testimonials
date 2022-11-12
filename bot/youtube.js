b8e0d260c400a0c3f5d99bb37642488c091b278b
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-12 16:27:12 +0800

    wrong folder

--- bot/youtube.js
+++ bot/youtube.js
@@ -16,7 +16,7 @@ function get(continuation) {
       get(data.continuation);
       if (i === 3) {
         console.log(`Downloaded ${comments.length} comments.`);
-        fs.writeFileSync("output/comments.json", JSON.stringify(comments));
+        fs.writeFileSync("comments.json", JSON.stringify(comments));
         process.exit();
       }
     });
